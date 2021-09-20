ea4e82d13e107ca5de15a798896f32b328ae1d5e
Olav van Rabenswaaij <38460642+OlavRab@users.noreply.github.com>
2021-09-20 13:00:53 +0200

    Update sensor.py

--- custom_components/rpitemp/sensor.py
+++ custom_components/rpitemp/sensor.py
@@ -1,1 +1,17 @@
-x
+import logging
+from datetime import date, datetime, timedelta
+
+import homeassistant.helpers.config_validation as cv
+import requests
+
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import ATTR_DATE, CONF_RESOURCES
+from homeassistant.helpers.entity import Entity
+
+__version__ = '0.1'
+
+_LOGGER = logging.getLogger(__name__)
+
+SENSOR_TYPES = {'today': ['Vandaag', 'mdi:thermometer']}
+
